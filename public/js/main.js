b250509868d182c6b36c60fc77d558f73c85f5b5
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-20 11:55:18 -0500

    Added basic routing and _boundInput

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,41 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function(){
-	m.mount(document.getElementById('app'), {
+m._boundInput = function(stream, attrs){
+	var attrs = (attrs || {});
+	attrs.value = stream();
+	attrs.oninput = function(event){
+		event.redraw = false;
+		m.withAttr('value', stream).call(null, event);
+	};
+	return attrs;
+}
+
+var Header = (function(){
+	return {
 		view: function(){
-			return m('p', 'This is Mithril!');
+			return [
+				m('h1', [
+					m('a', {
+						href: '/',
+						oncreate: m.route.link
+					}, 'Home')
+				])
+			];
 		}
+	}
+})();
+
+var Main = (function(){
+	return {
+		view: function(){
+			return m('p', 'Mithril is here!');
+		}
+	};
+})();
+
+document.addEventListener('DOMContentLoaded', function(){
+	m.mount(document.getElementById('header'), Header);
+	m.route(document.getElementById('main'), '/', {
+		'/': Main
 	});
 });
